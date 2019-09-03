691cc29d5f62f7cd5997b316b5c0db7bd78d51ce
MrRainx <diegocondo10@yahoo.es>
2019-09-02 23:36:17 -0500

    hice las pruebas pero solo genera una imagen de la pagina y la pega en el PDF :/

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,6 +3,7 @@ import { UsersService } from '../../services/users.service';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 
 @Component({
@@ -23,10 +24,21 @@ export class HomeComponent implements OnInit {
     this.user = this.userSrv.getUserLoggedIn();
   }
 
+
   generarPdf() {
-    const doc = new jsPDF();
-    doc.fromHTML(document.getElementById('home'), 10, 10);
-    doc.save('Reporte Prueba');
+
+    html2canvas(document.getElementById('home'), {
+      allowTaint: true,
+      useCORS: false,
+      scale: 1
+    }).then(function (canvas) {
+      var img = canvas.toDataURL("image/png");
+
+      var doc = new jsPDF();
+      doc.addImage(img, 'PNG', 7, 20, 195, 105);
+      doc.save('prueba.pdf')
+    })
+
   }
 
 }
