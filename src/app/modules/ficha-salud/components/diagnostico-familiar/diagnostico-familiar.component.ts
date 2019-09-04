4ff4e3cd3a88eb67dd6de164a2d4e734e2ef36f9
MrRainx <diegocondo10@yahoo.es>
2019-09-03 22:34:09 -0500

    agregando los diagnosticos de famila

--- src/app/modules/ficha-salud/components/diagnostico-familiar/diagnostico-familiar.component.ts
+++ src/app/modules/ficha-salud/components/diagnostico-familiar/diagnostico-familiar.component.ts
@@ -9,22 +9,62 @@ import { DetalleDiagnostico } from '../../../../models/detalle-diagnostico';
 export class DiagnosticoFamiliarComponent implements OnInit {
 
   @Input() diagnosticos: DetalleDiagnostico[]
-  public detail: DetalleDiagnostico = {
-    diagnostico: {
-      parentesco: '',
-      diagnostico: ''
-    }
-  }
+  public detail: DetalleDiagnostico = this.resetDetail()
+  public disableModalInput: boolean = false;
   public btn: string = 'Agregar';
+
+
   constructor() { }
 
   ngOnInit() {
 
   }
 
+  private resetDetail() {
+    return {
+      diagnostico: {
+        parentesco: '',
+        diagnostico: ''
+      }
+    }
+  }
+
   btnTbl(detail: DetalleDiagnostico, option: string) {
-    this.detail = detail;
+    this.detail = detail || this.resetDetail();
     this.btn = option
+
+    if (option == 'Eliminar') {
+      this.disableModalInput = true
+    } else {
+      this.disableModalInput = false;
+    }
+  }
+
+
+  btnModal(option: string) {
+
+
+
+    switch (option) {
+      case "Agregar"://Agregar
+        console.log("Agregar");
+
+        break;
+
+      case "Editar"://Editar
+        console.log("Editar");
+
+        break;
+
+      case "Eliminar"://Eliminar
+        console.log("Eliminar");
+        break;
+
+      default:
+        break;
+    }
+
+
   }
 
 }
